e8f09a797514323540d61a0810cb8d7e649d6288
mayorJAY <joseolu4gsm@yahoo.com>
2021-03-14 10:34:47 +0100

    feat: Refactor implementation

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,27 @@
 public class Main {
 
     public static void main(String[] args) {
-        int sum = 0;
-        // The array to work on
         int[] myArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
                 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
-        // Get each number from the array
-        for (int n : myArray) {
-            // Check to determine if it is a prime number
-            if (isPrime(n)) {
-                // Sum up the prime numbers
-                sum += n;
-                System.out.println(sum);
-            }
-        }
+        sumPrimeNumbers(myArray);
     }
 
-    private static boolean isPrime(int n) {
+    private static void sumPrimeNumbers(int[] numbers) {
+        int sum = 0;
+        for (int num: numbers) {
+            if (isPrimeNumber(num)) {
+                sum += num;
+            }
+        }
+        System.out.println(sum);
+    }
+
+    private static boolean isPrimeNumber(int n) {
         // Exclude 0 and 1 cos they are not prime numbers
-        if (n <= 1){
+        if (n <= 1) {
             return false;
         }
-        // Identify the actual prime numbers from the array
+        // Identify the actual prime numbers
         for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) {
                 return false;
